3a74932a409480fd368a4444da8edc5c182feed9
Jaqb02 <jakub.kurzawski.02@gmail.com>
2026-06-24 19:04:03 +0200

    gitignore proper position

--- FinanceTracker.Application/obj/Debug/net9.0/FinanceTracker.Application.AssemblyInfo.cs
+++ FinanceTracker.Application/obj/Debug/net9.0/FinanceTracker.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714a05415665cc7d14dc03d5cc32ab8855736377")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
